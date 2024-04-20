41a39cf8b60ec5cd552cb2981c7b90051ee53825
ryanlupague <ryan.lupague@indebted.co>
2024-04-20 09:52:28 +0800

    Add config

---
+++ config/contributions.py
@@ -0,0 +1,72 @@
+SSS = {
+    "2024": {
+        "Contribution Rate": 0.14,
+        "Employer": 0.095,
+        "Employee": 0.045,
+        "Minimum": 4000,
+        "Maximum": 30000,
+    },
+    "2025": {
+        "Contribution Rate": 0.15,
+        "Employer": 0.10,
+        "Employee": 0.05,
+        "Minimum": 5000,
+        "Maximum": 35000,
+    },
+}
+
+PHILHEALTH = {
+    "2024": {
+        "Contribution Rate": 0.05,
+        "Employer": 0.025,
+        "Employee": 0.025,
+        "Minimum": 10000,
+        "Maximum": 100000,
+    },
+    "2025": {
+        "Contribution Rate": 0.05,
+        "Employer": 0.025,
+        "Employee": 0.025,
+        "Minimum": 10000,
+        "Maximum": 100000,
+    },
+}
+
+PAGIBIG = {
+    "2024": [
+        {
+            "Contribution Rate": 0.03,
+            "Employer": 0.02,
+            "Employee": 0.01,
+            "Minimum": 0,
+            "Maximum": 1500,
+            "Salary Range": [0, 1500],
+        },
+        {
+            "Contribution Rate": 0.04,
+            "Employer": 0.02,
+            "Employee": 0.02,
+            "Minimum": 0,
+            "Maximum": 5000,
+            "Salary Range": [1500, float("inf")],
+        },
+    ],
+    "2025": [
+        {
+            "Contribution Rate": 0.04,
+            "Employer": 0.02,
+            "Employee": 0.02,
+            "Minimum": 0,
+            "Maximum": 5000,
+            "Salary Range": [0, 1500],
+        },
+        {
+            "Contribution Rate": 0.04,
+            "Employer": 0.02,
+            "Employee": 0.02,
+            "Minimum": 0,
+            "Maximum": 5000,
+            "Salary Range": [1500, float("inf")],
+        },
+    ],
+}
