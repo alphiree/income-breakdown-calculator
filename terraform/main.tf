e57aa28c50fbacc7673700a509445d0b697c4fd4
alphire-arch <rylupague@gmail.com>
2025-12-27 10:43:46 +0800

    define terraform configs

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = var.aws_region
+  profile = "ryan"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name = "app-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  tags = {
+    Name = "ssm-docker-app"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
